8f2f55504e1845e43b0624ce316b279a69a5b918
SurFerie20 <snegr8308@gmail.com>
2026-07-02 20:08:24 +0300

    Initialize WindUI and create main window with tabs
    Initialized WindUI and created main window with tabs for Player, Sheriff, Murder, Player Troll, Scripts, and About.

--- Main.lua
+++ Main.lua
@@ -1,1 +1,20 @@
+-- Инициализация WindUI (предполагается, что библиотека уже загружена)
+local WindUI = a.load('a') -- Загрузка основной библиотеки
+local Window = WindUI.Window -- Или ваша логика инициализации окна
+
+-- Создание главного окна
+local Main = WindUI:CreateWindow({
+    Title = "MM2 Profix Script",
+    Folder = "MM2Profix",
+})
+
+-- Создание разделов (Tabs)
+local TabPlayer = Main:CreateTab({ Title = "Player" })
+local TabSheriff = Main:CreateTab({ Title = "Sheriff" })
+local TabMurder = Main:CreateTab({ Title = "Murder" })
+local TabTroll = Main:CreateTab({ Title = "Player Troll" })
+local TabScripts = Main:CreateTab({ Title = "Scripts" })
+local TabAbout = Main:CreateTab({ Title = "About" })
+
+-- Разделы созданы, дальше вы сможете добавлять в них элементы
 
